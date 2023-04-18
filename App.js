e2c135849900ada493456266ea49aebe7189b564
JensHVF <117752914+JensHVF@users.noreply.github.com>
2023-04-18 13:51:03 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+//alert("Hello");
+
+function myFunction() {
+    //var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //document.getElementById("demo").style.color = "red";
+    alert("Hej!");
+}
+console.log("Hello");
